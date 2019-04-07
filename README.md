9db78218efbbd43d736b99c0f2ba9db87bed5aff
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-04-07 06:56:05 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # webkvalitet-api
+
 API for webquality
+
+[webkvalitet.api.alheimsins.net](https://webkvalitet.api.alheimsins.net)
+
+# Update
+
+Run the scripts
+
+```
+$ npm run build-partier
+```
+
+# Deploy
+
+```
+$ npm run deploy
+```
+
+# License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,27 @@
+(async () => {
+  process.env.debug = true
+  const { writeFile } = require('fs').promises
+  const wq = require('@alheimsins/webquality')
+  const sourceFile = process.argv[2]
+  let pages = require(`./sources/${sourceFile}`)
+  let data = []
+  console.log(`Got ${pages.length} pages`)
+  const next = async () => {
+    if (pages.length > 0) {
+      const now = new Date()
+      let page = pages.pop()
+      console.log(`Now checking: ${page.name}`)
+      page.date = now
+      page.timeStamp = now.getTime()
+      const result = await wq(page.url)
+      console.log(`Finished checking: ${page.name}`)
+      page.result = result
+      data.push(page) 
+      await next()
+    } else {
+      await writeFile(`data/${sourceFile}`, JSON.stringify(data, null, 2), 'utf-8')
+      console.log('Finished')
+    }
+  }
+  await next()
+})()
