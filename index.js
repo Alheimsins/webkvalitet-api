a6846c8388e1c35defa6706c8e8591aa8887e939
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-04-08 07:21:36 +0200

    Implements retry

--- index.js
+++ index.js
@@ -13,11 +13,21 @@
       console.log(`Now checking: ${page.name}`)
       page.date = now
       page.timeStamp = now.getTime()
-      const result = await wq(page.url)
-      console.log(`Finished checking: ${page.name}`)
-      page.result = result
-      data.push(page)
-      await next()
+      try {
+        const result = await wq(page.url)
+        console.log(`Finished checking: ${page.name}`)
+        page.result = result
+        data.push(page)
+        await next()
+      } catch (error) {
+        console.error(`Failed checking: ${page.name} - ${error}`)
+        page.retries = !page.retries ? 0 : page.retries++
+        if (page.retries < 3) {
+          // Adds page back in the queue
+          pages.unshift(page)
+        }
+        await next()
+      }
     } else {
       await writeFile(`data/${sourceFile}`, JSON.stringify(data, null, 2), 'utf-8')
       console.log('Finished')
