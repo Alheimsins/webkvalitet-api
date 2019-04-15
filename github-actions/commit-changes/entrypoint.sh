a32a1e65eaee6252970d0b90c755b43dca5da073
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-04-15 06:21:02 +0200

    Tries to fix action (patch)

--- github-actions/commit-changes/entrypoint.sh
+++ github-actions/commit-changes/entrypoint.sh
@@ -4,4 +4,7 @@ set -eu
 sh -c "git config --global user.name 'zrrrzzt' \
       && git config --global user.email 'geir.gasodden@pythonia.no' \
       && git commit -am '$*' --allow-empty \
+      && git branch my-temporary-work \
+      && git checkout master \
+      && git merge my-temporary-work \
       && git push -u origin master"
